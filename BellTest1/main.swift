8ff6c5bb567df79b18007239ead34264e6f978b0
Greg Wishart <greg.wishart@gmail.com>
2020-03-20 13:32:33 -0400

    Initial Commit

---
+++ BellTest1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BellTest1
+//
+//  Created by G Bear on 2020-03-20.
+//  Copyright © 2020 Rooh Bear Corporation. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
